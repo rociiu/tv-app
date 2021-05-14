8669d87e76b2a963484416c68c96ab79da10bba0
Roc Yu <rociiu.yu@gmail.com>
2021-05-14 20:14:05 +0800

    Add initial tv app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ReactHlsPlayer from 'react-hls-player';
 
-function App() {
+const playlistData = require('./playlist.json');
+
+const App = (props) => {
+  const [index, setIndex] = React.useState(0);
+
+  const handleKeyDown = (event) => {
+    if(event.keyCode === 39) {
+      setIndex(index + 1)
+    } else if (event.keyCode === 37) {
+      setIndex(index - 1)
+    } else if (event.keyCode === 38 || event.keyCode === 40) {
+      var randomIndex = Math.floor(Math.random() * (playlistData.length - 0 + 1) + 0);
+      setIndex(randomIndex)
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    // cleanup this component
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <ReactHlsPlayer
+          src={playlistData[index].url}
+          autoPlay={true}
+          controls={true}
+          width="100%"
+          height="auto"
+        />
+        <h2 style={{textAlign: 'center'}}>{ playlistData[index].name }</h2>
+      </div>
     </div>
   );
 }
